b8b2ddb103346312e01e8b14249917aa59a88879
eru764 <46580058+eru764@users.noreply.github.com>
2026-03-28 16:45:43 +0300

    feat: system tray background monitoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:window_manager/window_manager.dart';
+import 'package:tray_manager/tray_manager.dart';
+import 'dart:io';
 import 'screens/login_screen.dart';
 
 void main() async {
@@ -7,11 +10,107 @@ void main() async {
   
   await dotenv.load();
   
+  // Initialize window manager
+  await windowManager.ensureInitialized();
+  
+  WindowOptions windowOptions = const WindowOptions(
+    size: Size(800, 600),
+    center: true,
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    titleBarStyle: TitleBarStyle.normal,
+  );
+  
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+  
+  // Initialize system tray
+  await _initSystemTray();
+  
   runApp(const ClipSyncApp());
 }
 
-class ClipSyncApp extends StatelessWidget {
+Future<void> _initSystemTray() async {
+  // Set tray icon - use app icon from resources
+  String iconPath = Platform.isWindows
+      ? 'windows/runner/resources/app_icon.ico'
+      : 'assets/app_icon.png';
+  
+  await trayManager.setIcon(iconPath);
+  
+  // Set tooltip
+  await trayManager.setToolTip('ClipSync - Monitoring clipboard');
+  
+  // Create context menu
+  Menu menu = Menu(
+    items: [
+      MenuItem(
+        key: 'show_window',
+        label: 'Show ClipSync',
+      ),
+      MenuItem.separator(),
+      MenuItem(
+        key: 'quit',
+        label: 'Quit',
+      ),
+    ],
+  );
+  
+  await trayManager.setContextMenu(menu);
+}
+
+class ClipSyncApp extends StatefulWidget {
   const ClipSyncApp({super.key});
+
+  @override
+  State<ClipSyncApp> createState() => _ClipSyncAppState();
+}
+
+class _ClipSyncAppState extends State<ClipSyncApp> with TrayListener, WindowListener {
+  @override
+  void initState() {
+    super.initState();
+    trayManager.addListener(this);
+    windowManager.addListener(this);
+  }
+
+  @override
+  void dispose() {
+    trayManager.removeListener(this);
+    windowManager.removeListener(this);
+    super.dispose();
+  }
+
+  @override
+  void onTrayIconMouseDown() {
+    // Show window when tray icon is clicked
+    windowManager.show();
+  }
+
+  @override
+  void onTrayIconRightMouseDown() {
+    // Show context menu on right click
+    trayManager.popUpContextMenu();
+  }
+
+  @override
+  void onTrayMenuItemClick(MenuItem menuItem) {
+    if (menuItem.key == 'show_window') {
+      windowManager.show();
+      windowManager.focus();
+    } else if (menuItem.key == 'quit') {
+      windowManager.destroy();
+      exit(0);
+    }
+  }
+
+  @override
+  void onWindowClose() async {
+    // Hide to tray instead of closing
+    await windowManager.hide();
+  }
 
   @override
   Widget build(BuildContext context) {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,15 @@ import FlutterMacOS
 import Foundation
 
 import pasteboard
+import screen_retriever
 import shared_preferences_foundation
+import tray_manager
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PasteboardPlugin.register(with: registry.registrar(forPlugin: "PasteboardPlugin"))
+  ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  TrayManagerPlugin.register(with: registry.registrar(forPlugin: "TrayManagerPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
